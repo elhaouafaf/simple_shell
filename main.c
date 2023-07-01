45f3b741f4529c965fd9a6270bcd9cadff454027
afaf <128941203+elhaouafaf@users.noreply.github.com>
2023-07-01 08:34:31 +0200

    hope

--- main.c
+++ main.c
@@ -80,6 +80,17 @@ int main(void)
 		args = handle_input(&buf, &buf_size);
 		if (args == NULL)
 			continue;
+		if (!strcmp(args[0], "setenv") && args[1])
+   		{
+        	if (args[2])
+				_setenv(args[1], args[2], 1);
+        	exit(0);
+    	}
+    	if (!strcmp(args[0], "unsetenv") && args[1])
+    	{
+        _unsetenv(args[1]);
+        exit(0);
+    	}
 		if (strcmp(args[0], "exit") == 0)
 		{
 			free_args(args);
